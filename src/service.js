cda05f5bcc1f9dc5b844b62882333e14b4f7dfba
yogesh-u-k <110561274+yogesh-u-k@users.noreply.github.com>
2024-01-25 11:05:42 +0530

    Update service.js

--- src/service.js
+++ src/service.js
@@ -2,7 +2,7 @@ const queryStrings={
     app_id:process.env.REACT_APP_EDAMAN_ID,
     app_key:process.env.REACT_APP_EDAMAN_KEY
 }
-// https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=1637146e&app_key=72d21bf68b19e2247f62855d91086e00
+
 
 export const featchData = async (defaultQuery)=>{
     const {app_id, app_key} = queryStrings
